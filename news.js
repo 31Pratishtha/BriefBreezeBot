fde027a63924f0b9beb78837c843525e57e52cf2
Pratishtha Kumari <pratishtha.richu@gmail.com>
2024-06-27 00:26:55 +0530

    integrated the discord bot

--- news.js
+++ news.js
@@ -2,10 +2,11 @@
 import axios from "axios";
 const numOfArticles = 5;
 
+let articleTitles = [];
 const fetchNews = async () => {
     try {
         const res = await axios.get('https://newsapi.org/v2/top-headlines?category=business&category=science&category=technology&language=en&apiKey=a25a3356318a440396243f6e225333fc')
-        const articleTitles = [];
+        articleTitles = [];
         for(var i = 0; i < numOfArticles; i++){
             const article = res.data.articles[i];
             articleTitles.push({title: article.title, publishedDate:article.publishedAt, url: article.url});
@@ -16,4 +17,4 @@ const fetchNews = async () => {
     }
 }
 
-fetchNews();
+export {fetchNews, articleTitles};
